12aa9524623ee30ea87674e75ee9bc1884f4a38f
krishna <krishna@barefootnetworks.com>
2016-05-13 16:35:05 -0700

    tommy list delete fix for sflow

--- switchapi/src/switch_sflow.c
+++ switchapi/src/switch_sflow.c
@@ -208,14 +208,13 @@ switch_api_sflow_session_delete (switch_device_t device,
     }
     if (all_cleanup) {
         switch_sflow_match_entry_t* entry;
-        tommy_node *node = tommy_list_head(&sflow_info->match_list);
-        while (node) {
+        tommy_node *node = NULL;
+        while ((node = tommy_list_head(&sflow_info->match_list)) && node) {
             entry = (switch_sflow_match_entry_t *) node->data;
             // could be ingress or egress match entry 
             switch_pd_sflow_match_table_delete(device, entry);
             tommy_list_remove_existing(&sflow_info->match_list, node);
             switch_sflow_ace_handle_delete(entry->sflow_ace_hdl);
-            node = node->next;
         }
     }
 
