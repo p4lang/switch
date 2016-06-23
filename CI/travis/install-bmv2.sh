334ce8c8c3e8758b34b29d174b397d4b9e9b9659
Antonin Bas <antonin@barefootnetworks.com>
2016-06-23 16:10:56 -0700

    updated p4-build submodule ref pointer

--- CI/travis/install-bmv2.sh
+++ CI/travis/install-bmv2.sh
@@ -2,9 +2,9 @@
 set -e
 git clone https://github.com/p4lang/behavioral-model.git bmv2_tmp
 cd bmv2_tmp
-bash build/travis/install-nanomsg.sh
+bash travis/install-nanomsg.sh
 sudo ldconfig
-bash build/travis/install-nnpy.sh
+bash travis/install-nnpy.sh
 ./autogen.sh
 ./configure 'CXXFLAGS=-O0' --with-pdfixed
 make -j2 && sudo make install
