bc26ff73fd9434309ea52c92be00ebae882da86a
Pierce McEntagart <pierce.mcentagart@barefootnetworks.com>
2016-07-27 15:26:38 -0700

    Update startup script.

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,11 +5,11 @@ sudo /switch/tools/veth_setup.sh
 sleep 10
 
 # start bm
-sudo ./switch/docker/run_bm.sh &>/dev/null &
+sudo ./switch/docker/run_bm.sh &>bmlog &
 sleep 10
-sudo ./switch/docker/run_drivers.sh $@ &>/dev/null &
+sudo ./switch/docker/run_drivers.sh $@ &>driverlog &
 sleep 10
 
-#configure port_vlan_mapping entries
+# configure port_vlan_mapping entries
 sudo ./switch/docker/configure.sh &>/dev/null &
 sleep 2
